9d9d4f4c632c7500232d377ed046c79836c17d1a
Jack Draper <drapj002@gmail.com>
2023-11-02 23:22:51 +0000

    fix: incorrect serverless package

--- dislord/serverless.py
+++ dislord/serverless.py
@@ -5,7 +5,7 @@ from .interaction import interaction
 
 def serverless_handler(event, context):
     if event['httpMethod'] == "POST":
-        raw_request = json.loads(event.body)
+        raw_request = json.loads(event["body"])
         print(f"👉 Request: {raw_request}")
         return {"statusCode": "200",
                 "body": interaction.interact(raw_request),
