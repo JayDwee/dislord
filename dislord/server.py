d6b1c76f8f979822bc0d62494a2591f293e86dd7
Jack Draper <drapj002@gmail.com>
2023-11-03 00:11:45 +0000

    chore: calculate signature for serverless

--- dislord/server.py
+++ dislord/server.py
@@ -8,8 +8,10 @@ app = Flask(__name__)
 @app.route("/", methods=["POST"])
 async def interactions_endpoint():
     raw_request = request.json
+    signature = request.headers.get('X-Signature-Ed25519')
+    timestamp = request.headers.get('X-Signature-Timestamp')
     print(f"👉 Request: {raw_request}")
-    return interaction.interact(raw_request)
+    return interaction.interact(raw_request, signature, timestamp).as_server_response()
 
 
 def start_server():
--- dislord/serverless.py
+++ dislord/serverless.py
@@ -5,8 +5,10 @@ from .interaction import interaction
 
 def serverless_handler(event, context):
     if event['httpMethod'] == "POST":
+        print(f"🫱 Full Event: {event}")
         raw_request = json.loads(event["body"])
         print(f"👉 Request: {raw_request}")
-        return {"statusCode": "200",
-                "body": interaction.interact(raw_request),
-                "headers": {"Content-Type": "application/json"}}
+        raw_headers = json.loads(event["headers"])
+        signature = raw_headers.get('X-Signature-Ed25519')
+        timestamp = raw_headers.get('X-Signature-Timestamp')
+        return interaction.interact(raw_request, signature, timestamp).as_serverless_response()
