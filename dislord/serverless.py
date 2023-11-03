ce2976088083b41a7ad202413dbbeab51d24a179
Jack Draper <drapj002@gmail.com>
2023-11-03 00:18:00 +0000

    fix: remove json loads

--- dislord/serverless.py
+++ dislord/serverless.py
@@ -8,7 +8,7 @@ def serverless_handler(event, context):
         print(f"🫱 Full Event: {event}")
         raw_request = json.loads(event["body"])
         print(f"👉 Request: {raw_request}")
-        raw_headers = json.loads(event["headers"])
+        raw_headers = event["headers"]
         signature = raw_headers.get('X-Signature-Ed25519')
         timestamp = raw_headers.get('X-Signature-Timestamp')
         return interaction.interact(raw_request, signature, timestamp).as_serverless_response()
